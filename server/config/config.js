bf732d231b4dd94f82ad18d57c05797c6d9f18b2
ElmerYDQ <elmer.04.dq@gmail.com>
2020-02-22 12:36:29 -0500

    varible de entrono

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:bBxfQ5z088Lz5i2y@cluster0-ns9bo.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
